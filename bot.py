64f295975e0fb1064c536cdacc853893b2daf8f3
gamernoob59 <70986772+gamernoob59@users.noreply.github.com>
2021-11-08 19:43:25 -0500

    updated permissions workings and status
    no longer need to manually put in an admin role, anyone with administrator permissions can change the reporting channel, and now has a listening status

--- bot.py
+++ bot.py
@@ -9,38 +9,41 @@ client = commands.Bot(command_prefix="%")
 @client.event
 async def on_ready():
     print("Logged in as " + client.user.name)
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name='To Your Words'))
 
 
 #Sends an error message when a nonexistant command is run
 @client.event
 async def on_command_error(ctx, error):
-    embed=discord.Embed(title="Error", description="This command does not exist or is incorrectly typed. Use the help command if you need assistance.", color=discord.Color.red())
+    embed=discord.Embed(title="Error", description="This command does not exist or you do not have nessesary permissions. Use the help command if you need assistance.", color=discord.Color.red())
     await ctx.send(embed=embed)
 
 @client.command(
     help="Set the channel ID that the social credit notices are reported to.",
     brief="Set the reporting channel for sc"
 )
-
+@commands.has_permissions(administrator=True)
 #This command lets anyone with the specified role to change the reporting channel ID
 async def setchannelID(ctx, cID):
-    staff_role = discord.utils.get(ctx.guild.roles, id=902327651442786314)
-    if staff_role in ctx.author.roles:
         global channelId
         channelId=int(cID)
-        await ctx.channel.send("Reporting channel ID is set as: "+cID)
-    else:
-        embed=discord.Embed(title="Error", description="You need the **Admin** role to run this command.", color=discord.Color.red())
-        await ctx.send(embed=embed)
-
+        await ctx.channel.send("The reporting channel ID for infractions is now set as: "+cID)
 
 #These are all the phrases that can trigger a response, copy and paste within the function and change the wording to add more
 @client.event
 async def on_message(message):
-    
+    if message.content.startswith('china commits'):
+        channel = client.get_channel(channelId)
+        content=message.content
+        await message.channel.send('**MESSAGE REPORTED**')
+        await channel.send('ILLEGAL KEYWORD DETECED BY:')
+        await channel.send(message.author)
+        await channel.send("MESSAGE: "+content)
+
     if message.content.startswith('taiwan is a country'):
         channel = client.get_channel(channelId)
         content=message.content
+        await message.channel.send('**MESSAGE REPORTED**')
         await channel.send('ILLEGAL KEYWORD DETECED BY:')
         await channel.send(message.author)
         await channel.send("MESSAGE: "+content)
@@ -48,6 +51,7 @@ async def on_message(message):
     if message.content.startswith('something happened in tianamen square in 1989'):
         channel = client.get_channel(channelId)
         content=message.content
+        await message.channel.send('**MESSAGE REPORTED**')
         await channel.send('ILLEGAL KEYWORD DETECED BY:')
         await channel.send(message.author)
         await channel.send("MESSAGE: "+content)
@@ -55,6 +59,7 @@ async def on_message(message):
     if message.content.startswith('something happened in tianamen square'):
         channel = client.get_channel(channelId)
         content=message.content
+        await message.channel.send('**MESSAGE REPORTED**')
         await channel.send('ILLEGAL KEYWORD DETECED BY:')
         await channel.send(message.author)
         await channel.send("MESSAGE: "+content)
@@ -62,6 +67,7 @@ async def on_message(message):
     if message.content.startswith('david sucks'):
         channel = client.get_channel(channelId)
         content=message.content
+        await message.channel.send('**MESSAGE REPORTED**')
         await channel.send('ILLEGAL KEYWORD DETECED BY:')
         await channel.send(message.author)
         await channel.send("MESSAGE: "+content)
@@ -69,6 +75,7 @@ async def on_message(message):
     if message.content.startswith('david is great'):
         channel = client.get_channel(channelId)
         content=message.content
+        await message.channel.send('**POSITIVE MESSAGE REPORTED**')
         await channel.send('POSITIVE KEYWORD DETECED BY:')
         await channel.send(message.author)
         await channel.send("MESSAGE: "+content)
@@ -76,6 +83,7 @@ async def on_message(message):
     if message.content.startswith('i love david'):
         channel = client.get_channel(channelId)
         content=message.content
+        await message.channel.send('**POSITIVE MESSAGE REPORTED**')
         await channel.send('POSITIVE KEYWORD DETECED BY:')
         await channel.send(message.author)
         await channel.send("MESSAGE: "+content)
@@ -83,6 +91,7 @@ async def on_message(message):
     if message.content.startswith('hail the ccp'):
         channel = client.get_channel(channelId)
         content=message.content
+        await message.channel.send('**POSITIVE MESSAGE REPORTED**')
         await channel.send('POSITIVE KEYWORD DETECED BY:')
         await channel.send(message.author)
         await channel.send("MESSAGE: "+content)
@@ -90,6 +99,7 @@ async def on_message(message):
     if message.content.startswith('hail the CCP'):
         channel = client.get_channel(channelId)
         content=message.content
+        await message.channel.send('**POSITIVE MESSAGE REPORTED**')
         await channel.send('POSITIVE KEYWORD DETECED BY:')
         await channel.send(message.author)
         await channel.send("MESSAGE: "+content)
@@ -97,6 +107,7 @@ async def on_message(message):
     if message.content.startswith('china is the best'):
         channel = client.get_channel(channelId)
         content=message.content
+        await message.channel.send('**POSITIVE MESSAGE REPORTED**')
         await channel.send('POSITIVE KEYWORD DETECED BY:')
         await channel.send(message.author)
         await channel.send("MESSAGE: "+content)
@@ -104,6 +115,7 @@ async def on_message(message):
     if message.content.startswith('hail david'):
         channel = client.get_channel(channelId)
         content=message.content
+        await message.channel.send('**POSITIVE MESSAGE REPORTED**')
         await channel.send('POSITIVE KEYWORD DETECED BY:')
         await channel.send(message.author)
         await channel.send("MESSAGE: "+content)
